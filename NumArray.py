87006bb4f0f94fb75f72e849dcd037f2763a2d58
anu-ship-it <anup17508@gmail.com>
2026-08-02 17:38:54 +0530

    Done with numpy

--- NumArray.py
+++ NumArray.py
@@ -4,3 +4,19 @@ val = array([1, 2, 3, 4.5,'a'])
 
 for x in val:
     print(x, end=' ')
+
+print("\n")
+# Creating an array using linspace
+val1 = linspace(10, 20, 5)  # 5 numbers between 10 and 20
+
+for x in val1:
+    print(x, end=' ')
+
+print("\n")
+# Creating an array using arange
+val2 = arange(0, 10, 2)  # Numbers from 0 to 10 with a step of 2
+
+for x in val2:
+    print(x, end=' ')
+
+print("\n")
