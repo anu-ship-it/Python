8fac478229f11a6be7b277b424773261987a023c
anu-ship-it <anup17508@gmail.com>
2026-08-02 18:02:00 +0530

    Added multi Dim array

---
+++ MulArray.py
@@ -0,0 +1,20 @@
+from numpy import *
+
+# Zero Dimensional array
+zero = array(10)
+print(zero)
+
+print('\n')
+# One Dimensional array 
+one = array([1, 2, 3, 4, 5])
+print(one)
+
+print('\n')
+# Two Dimensional array
+two = array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(two)
+
+print('\n')    
+# Three Dimensional array
+three = array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(three)
